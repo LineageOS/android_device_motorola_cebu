09ef05381f4d1ecb40ca8098537ac5701e0a33bc
Wolfram Liebchen <achalmgucker@googlemail.com>
2023-10-21 22:35:20 +0200

    cebu: Enable notification light
    The otherwise unused charging-LED is used as notification light with
    configurable pulse-widths.
    
    Change-Id: I4fe9b8c56b8735135939f8a1db3908d20d871bc2

--- device.mk
+++ device.mk
@@ -21,13 +21,18 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.bengal
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
