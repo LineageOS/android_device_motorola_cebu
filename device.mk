abe17c164bdd77eb3780a871d18ca81cbbf7b2c9
Michael Bestas <mkbestas@lineageos.org>
2025-06-22 17:35:54 -0400

    cebu: overlay: Convert overlays to Android.bp
    Also run everything through beautify_rro.py
    
    Change-Id: I10a3a9e459fc6c512b273194a8ace6159106aaa1

--- device.mk
+++ device.mk
@@ -41,9 +41,10 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    FrameworksResDevice \
+    LineagePlatformDevice \
+    SystemUIResDevice
 
 # Permissions
 PRODUCT_COPY_FILES += \
